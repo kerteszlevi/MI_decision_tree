9add921e16251d4d1fe2d6b6c9aaa276711ff5aa
kerteszlevi <kerteszlevi@gmail.com>
2023-11-24 10:25:11 +0100

    init

---
+++ solution.py
@@ -0,0 +1,23 @@
+import numpy as np #(működik a Moodle-ben is)
+
+
+######################## 1. feladat, entrópiaszámítás #########################
+def get_entropy(n_cat1: int, n_cat2: int) -> float:
+    entropy = 0
+    #TODO: számítsa ki az entrópiát!
+    return entropy
+
+###################### 2. feladat, optimális szeparáció #######################
+def get_best_separation(features: list,
+                        labels: list) -> (int, int):
+    best_separation_feature, best_separation_value = 0, 0
+    #TODO számítsa ki a legjobb szeparáció tulajdonságát és értékét!
+    return best_separation_feature, best_separation_value
+
+################### 3. feladat, döntési fa implementációja ####################
+def main():
+    #TODO: implementálja a döntési fa tanulását!
+    return 0
+
+if __name__ == "__main__":
+    main()
